148d4a96b8f6c277ea368e821d3e9ae754faae26
Yoshinori Morita <morita.non+github@gmail.com>
2020-09-02 14:23:19 +0900

    構文木、再帰下降構文分析導入、スタックマシン実装

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+expr    = mul ("+" mul | "-" mul)*
+mul     = primary ("*" primary | "/" primary)*
+primary = num | "(" expr ")"
+**/
+
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;  // 左辺
+    Node *rhs;  // 右辺
+    int  val;   // Nodeが数値の場合のみ使用する。
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = (Node*)calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+
+Node *new_node_num(int val) {
+    Node *node = (Node*)calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->lhs = NULL;
+    node->rhs = NULL;
+    node->val = val;
+    return node;
+}
+
+Node *mul();
+
+
 typedef enum {
     TK_RESERVED = 0,  // 記号
     TK_NUM,           // 整数トークン
@@ -48,6 +94,16 @@ bool consume(char op) {
     }
     token = token->next;
     return true;
+}
+
+/**
+ * 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+ * それ以外の場合にはエラーを報告する。
+ */
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error_at(token->str, "'%c'ではありません", op);
+  token = token->next;
 }
 
 /**
@@ -96,7 +152,9 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' ||
+            *p == '*' || *p == '/' ||
+            *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -113,6 +171,75 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+    return NULL;
+}
+
+Node *primary() {
+    // 次のトークンが"("なら、"(" expr ")" であるはず
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    //そうでないなら数値
+    return new_node_num(expect_number());
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for(;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_SUB, node, primary());
+        }
+        return node;
+    }
+    return NULL;
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch(node->kind) {
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("    imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+        break;
+    }
+    printf("    push rax\n");
+}
 
 int main(int argc, char **argv)
 {
@@ -125,24 +252,15 @@ int main(int argc, char **argv)
     user_input = argv[1];
     // トークナイズする。
     token = tokenize(user_input);
+    Node *node  = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので、それをチェックして最初のmov命令発行
-    printf("    mov rax, %d\n", expect_number());
+    gen(node);
 
-    // `+ <数値>` または、`- <数値>` というトークンの並びを消費しつつ
-    // アセンブリを出力
-    while(!at_eof()) {
-        if (consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-        consume('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
